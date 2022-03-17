626f487a22b6bac5bc0098b28fab0fbc280db70a
student <student@mini-07.local>
2022-03-17 11:11:03 +0100

    zadanie 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
+#include<trygonometria.h>
+
 
 int main() { 
-    std::cout<<"Hello World \n";
+    std::cout<<"Hello World :) \n";
+    double result = degreemath::sin(1.0);
+    std::cout<<result<<std::endl;
   return 0; }
---
+++ src/trygonometria/trygonometria.cpp
@@ -0,0 +1,22 @@
+#include "trygonometria.h"
+#include <cmath>
+
+double degreemath::sin(double degree) {
+         double rad = degree*(M_PI/180); 
+         return std::sin(rad);
+}
+
+double degreemath::cos(double degree) {
+         double rad = degree*(M_PI/180); 
+         return std::cos(rad);
+}
+
+double degreemath::tan(double degree) {
+         double rad = degree*(M_PI/180); 
+         return std::tan(rad);
+}
+
+double degreemath::ctan(double degree) {
+         double rad = degree*(M_PI/180); 
+         return 1/std::tan(rad);
+}
